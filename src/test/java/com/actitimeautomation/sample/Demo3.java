d908d160778a09ac57e49fa5164d6991fbed4011
username <nishigandha1412@gmail.com>
2024-09-05 14:31:20 +0530

    Demo3 test

---
+++ src/test/java/com/actitimeautomation/sample/Demo3.java
@@ -0,0 +1,22 @@
+package com.actitimeautomation.sample;
+
+import com.actitimeautomation.common.BaseClass;
+import com.actitimeautomation.common.PropertyHandling;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class Demo3 extends BaseClass {
+    WebDriver driver;
+    PropertyHandling propertyHandling;
+    @Test
+    public void setUp() throws IOException {
+        propertyHandling=new PropertyHandling();
+       String browser= propertyHandling.getProperty("browser");
+       launchBrowser(browser);
+       driver=super.driver;
+       String url= propertyHandling.getProperty("actitime");
+       driver.get(url);
+    }
+}
